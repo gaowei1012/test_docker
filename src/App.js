c6a5a2f5576f55bd006982a75392d8457a096127
gmw-zjw <mingwei2222@126.com>
2019-06-20 00:40:13 +0800

    !

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Wclome Docker for React
+          Wclome Docker for React!
         </p>
         <a
           className="App-link"
