54fd9e16696245f231164af2f17483315c7a7467
gmw-zjw <mingwei2222@126.com>
2019-06-20 13:33:10 +0800

    + app

--- src/App.js
+++ src/App.js
@@ -4,21 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wclome Docker for React!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>欢迎使用本系统!!!</p>
     </div>
   );
 }
